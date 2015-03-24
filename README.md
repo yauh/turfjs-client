e8ba57d29c4c7de4df4659ab498e476b263a6d3a
Stephan Hochhaus <stephan@yauh.de>
2015-03-24 16:04:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Turfjs-client for Meteor
+========================
+
+This package is a simple wrapper for
+[Turf](https://github.com/Turfjs/turf). Manipulate GeoJSON objects,
+similar to PostGIS.
+
+Read [an introduction to Turf.js and spatial
+analysis](https://www.mapbox.com/guides/intro-to-turf/).
+
+Usage
+-----
+
+Install the package via command line:
+
+    $ meteor add yauh:turfjs-client
+
+**Note** `turf` is only available on the client, not on the server. If
+you need it on the server install `yauh:turfjs`.
+
+See [the official docs](http://turfjs.org/static/docs/) for the correct
+syntax.
---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'yauh:turfjs-client',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Turf.js library for geospatial operations (analyze, aggregate, and transform GeoJSON data)',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/yauh/turfjs-client',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.0.4.1');
+  api.export('Turf');
+  api.addFiles([
+      'turf.min.js'
+    ],
+    'client');
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+  api.use(['yauh:turfjs-client', 'templating', 'ui'], 'client');
+  api.addFiles([
+    'turfjs-tests.js'
+  ], 'client');
+});
