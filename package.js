1e1dde58882102786154c3dfa357f2337a270fd9
Stephan Hochhaus <stephan@yauh.de>
2015-04-04 15:31:09 +0200

    fix bug to work with server package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'yauh:turfjs-client',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Turf.js library for geospatial operations (analyze, aggregate, and transform GeoJSON data)',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,6 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0.4.1');
-  api.export('Turf');
   api.addFiles([
       'turf.min.js'
     ],
