7ebe279b28a657ceafd8c854dd8548248a384d87
Stephan Hochhaus <stephan@yauh.de>
2015-06-10 11:37:28 +0200

    update to turf 2.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'yauh:turfjs-client',
-  version: '0.0.2',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Turf.js library for geospatial operations (analyze, aggregate, and transform GeoJSON data)',
   // URL to the Git repository containing the source code for this package.
